ff567cf705a9a563126b60262ef0d5bccbdd4652
Jakub Kępinski <jakubkepinski87@gmail.com>
2022-11-03 21:03:14 +0100

    started mainsite

---
+++ komis/src/sites/MainSite.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react';
+
+function MainSite() {
+
+	return (
+        <>
+            <div className='nav_bar'>
+                <div className='nav_button'></div>
+                <div className='nav_button'></div>
+                <div className='nav_button'></div>
+                <div className='nav_button'></div>
+            </div>
+        </>
+    );
+}
+
+export default MainSite;
